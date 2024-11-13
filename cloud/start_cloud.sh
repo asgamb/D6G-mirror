89325c8b455193094efe24dbe8ee79fddd9c3fbc
Andrea Sgambelluri <andrea.sgambelluri@gmail.com>
2024-11-13 23:54:22 +0100

    re-added local_ip variable

--- cloud/start_cloud.sh
+++ cloud/start_cloud.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#filename=".env"
-#ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+filename=".env"
+ip=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 
-#echo $ip
+echo $ip
 
 
-#line="LOCAL_IP=${ip}"
-#echo "${line}"
+line="LOCAL_IP=${ip}"
+echo "${line}"
 
-#printf $line > $filename
+printf $line > $filename
 sudo docker compose -f cloud_dc.yml up -d
